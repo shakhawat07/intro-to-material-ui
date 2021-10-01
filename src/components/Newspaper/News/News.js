2513bf1d75d2396101f4e96dc375334f86d99477
shakhawat07 <shakhawathossainmridha@gmail.com>
2021-10-01 21:52:25 +0600

    first commit

---
+++ src/components/Newspaper/News/News.js
@@ -0,0 +1,36 @@
+import { Avatar, Card, CardContent, CardHeader, CardMedia, IconButton, Typography } from '@mui/material';
+import React from 'react';
+
+const News = (props) => {
+    const { title, description, urlToImage } = props.article;
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+
+                    </IconButton>
+                }
+                title={title}
+                subheader="September 14, 2016"
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={urlToImage}
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {description}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+};
+export default News;
